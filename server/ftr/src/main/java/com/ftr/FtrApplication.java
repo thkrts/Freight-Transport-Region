c872d1f1d89da58ac318d698797217220f40f76c
thkrts <tarunsingh08042000@gmail.com>
2025-03-25 18:06:36 +0530

    initial commit for server files

---
+++ server/ftr/src/main/java/com/ftr/FtrApplication.java
@@ -0,0 +1,13 @@
+package com.ftr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtrApplication.class, args);
+	}
+
+}
---
+++ server/ftr/src/test/java/com/ftr/FtrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
